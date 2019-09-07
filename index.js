05e28b292ce0ea2a6330a3b8d5f9cf8d617a4afe
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-07 22:26:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,3 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = `Welcome, ${currentUser}`;
-
-shortGreeting
-  contains the first initial of the name stored in the 'currentUser' variable
-    AssertionError: expected 'Welcome, ' to contain 'G'
